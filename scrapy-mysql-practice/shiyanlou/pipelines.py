4cf55c506d87bcf5e462418d2e68c3ad97aafd4b
Jasonjiang27 <jiangyz1991@163.com>
2018-06-09 17:25:43 +0800

    scrapy to mysql

---
+++ scrapy-mysql-practice/shiyanlou/pipelines.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from sqlalchemy.orm import sessionmaker
+from shiyanlou.models import Course,engine
+
+
+class ShiyanlouPipeline(object):
+    def process_item(self, item, spider):
+        item['students']=int(item['students'])
+        if item['students'] < 1000:
+            raise DropItem('Course student less than 1000.')
+        else:
+            self.session.add(Course(**item))
+        return item
+
+    def open_spider(self,spider):
+       """当爬虫被开启的时候调用
+       """
+       Session = sessionmaker(bind=engine)
+       self.session = Session()
+    
+
+    def close_spider(self,spider):
+        """当爬虫被关闭的时候调用
+        """
+        self.session.commit()
+        self.session.close()
+        pass
+
